45867687ccd461ad3030fb5b3c09811646376183
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-12-12 15:49:01 -0500

    Add movie details page

---
+++ pages/movies/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute()
+const router = useRouter()
+const id = $computed(() => route.params.id)
+let item = $ref({})
+
+onMounted(async () => {
+  const data = await byId(id)
+  item = data
+})
+</script>
+
+<template>
+  <div class="max-w-3xl mx-auto p-4">
+    <button type="button" class="flex items-center space-x-2" @click="router.back">
+      <Icon name="carbon:arrow-left" class="w-8 h-8 text-blue-900" />
+      <span class="text-blue-900">Go Back</span>
+    </button>
+    <MovieDetails :item="item" />
+  </div>
+</template>
