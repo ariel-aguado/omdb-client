376d56ca22c1516ebcf154e5af6a4ee2b572b892
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-12-12 15:48:05 -0500

    Add movies search page

---
+++ pages/movies/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <Suspense>
+      <SearchResults />
+      <template #fallback>
+        <div class="opacity-50 italic">
+          <span class="animate-pulse">Loading...</span>
+        </div>
+      </template>
+    </Suspense>
+  </div>
+</template>
