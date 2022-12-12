4848a325e2afd87c22351573bbe22854c10b4df9
Ariel Aguado Albear <ariel.841207@gmail.com>
2022-12-12 15:48:31 -0500

    Add composable for fetching movies

---
+++ composables/omdb.ts
@@ -0,0 +1,28 @@
+const apiBaseUrl = 'http://www.omdbapi.com/'
+const apiKey = 'd2e9bfa0'
+
+function _fetchOMDB(url, params) {
+  return $fetch(url, {
+    baseURL: `${apiBaseUrl}`,
+    params: {
+      apiKey,
+      ...params,
+    },
+  })
+}
+
+/**
+ * Search (searches movies, series and episode)
+ */
+
+export function bySearch(s, page = 1) {
+  return _fetchOMDB('/', { s, page })
+}
+
+/**
+ * Search by A valid IMDb ID (e.g. tt1285016)
+ */
+
+export function byId(i) {
+  return _fetchOMDB('/', { i })
+}
